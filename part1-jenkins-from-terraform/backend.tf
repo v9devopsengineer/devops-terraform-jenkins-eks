5b994b34fe270384d24ba71e8d5e5b469f4c5570
v9devopsengineer <112998990+v9devopsengineer@users.noreply.github.com>
2024-01-23 01:22:21 +0530

    Update backend.tf

--- part1-jenkins-from-terraform/backend.tf
+++ part1-jenkins-from-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mubin-devops-cicd-terraform-eks"
+    bucket = "tf-jenkins-devops-cicd-terraform-eks"
     region = "us-east-1"
     key = "jenkins-server/terraform.tfstate"
   }
