fee8a93c56dadae7e2526917b040b7dc513914ab
v9devopsengineer <112998990+v9devopsengineer@users.noreply.github.com>
2024-01-23 11:14:18 +0530

    Update backend.tf

--- part2-cluster-from-terraform-and-jenkins/terraform-for-cluster/backend.tf
+++ part2-cluster-from-terraform-and-jenkins/terraform-for-cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mubin-devops-cicd-terraform-eks"
-    region = "us-east-1"
+    bucket = "terraform-jenkins-devops-cicd-terraform-eks"
+    region = "eu-west-1"
     key = "eks/terraform.tfstate"
   }
 }
